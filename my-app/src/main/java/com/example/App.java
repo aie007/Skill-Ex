b01af2da5b0d617bd74cfd6e9f350b180f41b706
Aieshah <aieshah_nasir@hotmail.com>
2026-04-19 10:57:31 +0530

    add web app, playwright support

---
+++ my-app/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+import com.microsoft.playwright.*;
+
+public class App {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.firefox().launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+}
